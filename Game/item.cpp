f279f74ad95c18f600b142d16b20e056d5e44ed8
somamizuki <somamizuki@gmail.com>
2018-05-03 15:09:48 +0900

    tuika

---
+++ Game/item.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "item.h"
+
+
+item::item()
+{
+}
+
+
+item::~item()
+{
+}
+
+bool item::Start()
+{
+	for (int i = 0;i < tekikazu;i++)
+	{
+		tp = FindGO<tekihei>("tekihei");
+		itemf[i] = 0;
+		itemModelData.Load(L"modelData/item.cmo");
+		itemModel[i].Init(itemModelData);
+	}
+	
+	
+	return true;
+}
+
+void item::Update()
+{
+	for (int i = 0;i < tekikazu;i++)
+	{
+		if (tp != NULL)
+		{
+			if (tp->tekiheiflag[i] == 0)
+			{
+				itemf[i] = 1;
+				itempos[i] = tp->tekipos[i];
+			}
+		}
+		if(itemf[i]==1)
+		itemModel[i].Update(itempos[i], itemrot[i], CVector3::One);
+	}
+	
+}
+
+void item::Render(CRenderContext& rc)
+{
+	for (int i = 0;i < tekikazu;i++)
+	{
+		if(itemf[i]==1)
+		itemModel[i].Draw(rc, MainCamera().GetViewMatrix(), MainCamera().GetProjectionMatrix());
+	}
+		
+}
---
+++ Game/item.h
@@ -0,0 +1,20 @@
+#pragma once
+#include"tekihei.h"
+#define tekikazu 10
+class item:public IGameObject
+{
+public:
+	item();
+	~item();
+	bool Start();
+	void Update();
+	void Render(CRenderContext& rc);
+	CSkinModel itemModel[tekikazu];					//スキンモデル。
+	CSkinModelData itemModelData;			//スキンモデルデータ。
+	CVector3 itempos[tekikazu];
+	CQuaternion itemrot[tekikazu];
+	int itemf[tekikazu];
+	tekihei *tp;
+	
+};
+
